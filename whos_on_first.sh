31763b1dde1bddca94a952fd695977a27260fb6b
Brian Roth <brian.roth@infor.com>
2016-06-14 14:01:33 -0500

    first commit

---
+++ whos_on_first.sh
@@ -0,0 +1,401 @@
+#!/bin/sh
+
+CLS() { 
+  osascript -e 'tell application "System Events" to keystroke "k" using command down' 
+}
+
+ABBOT () {
+   /usr/bin/say -i -r 200 -v Daniel $1
+   CLS
+}
+
+COSTELLO () {
+   /usr/bin/say -i -r 200 -v Samantha $1
+   CLS
+}
+
+PAUSE () {
+  sleep 1
+}
+
+CLS
+
+ABBOT "You know Bucky Harris, the Yankee's manager?  He gave me a job as coach for as long as you're on the team."
+
+COSTELLO "If you're the coach, you must know all the players."
+
+ABBOT "I certainly do."
+
+COSTELLO "Well you know I've never met the guys. So you'll have to tell me their names, and then I'll know who's playing on the team."
+
+ABBOT "Oh, I'll tell you their names, but you know it seems to me they give these ball players now-a-days very peculiar names."
+
+COSTELLO "You mean funny names?"
+
+ABBOT "Strange names, pet names...like Dizzy Dean..."
+
+COSTELLO "His brother Daffy."
+
+ABBOT "Daffy Dean..."
+
+COSTELLO "And their French cousin."
+
+ABBOT "French?"
+
+COSTELLO "Goofey."
+
+ABBOT "Goofey Dean. Well, let's see, we have on the bags, Who's on first, What's on second, I Don't Know is on third..."
+
+COSTELLO "That's what I want to find out."
+
+ABBOT "I say Who's on first, What's on second, I Don't Know's on third."
+
+COSTELLO "Are you the manager?"
+
+ABBOT "Yes."
+
+COSTELLO "You gonna be the coach too?"
+
+ABBOT "Yes."
+
+COSTELLO "And you don't know the fellows' names?"
+
+ABBOT "Well I should."
+
+COSTELLO "Well then who's on first?"
+
+ABBOT "Yes."
+
+COSTELLO "I mean the fellow's name."
+
+ABBOT "Who."
+
+COSTELLO "The guy on first."
+
+ABBOT "Who."
+
+COSTELLO "The first baseman."
+
+ABBOT "Who."
+
+COSTELLO "The guy playing..."
+
+ABBOT "Who is on first!"
+
+COSTELLO "I'm asking YOU who's on first."
+
+ABBOT "That's the man's name."
+
+COSTELLO "That's who's name?"
+
+ABBOT "Yes."
+
+COSTELLO "Well go ahead and tell me."
+
+ABBOT "That's it."
+
+COSTELLO "That's who?"
+
+ABBOT "Yes."
+
+PAUSE
+
+COSTELLO "Look, you gotta first baseman?"
+
+ABBOT "Certainly."
+
+COSTELLO "Who's playing first?"
+
+ABBOT "That's right."
+
+COSTELLO "When you pay off the first baseman every month, who gets the money?"
+
+ABBOT "Every dollar of it."
+
+COSTELLO "All I'm trying to find out is the fellow's name on first base."
+
+ABBOT "Who."
+
+COSTELLO "The guy that gets..."
+
+ABBOT "That's it."
+
+COSTELLO "Who gets the money..."
+
+ABBOT "He does, every dollar. Sometimes his wife comes down and collects it."
+
+COSTELLO "Who's wife?"
+
+ABBOT "Yes."
+
+PAUSE
+
+ABBOT "What's wrong with that?"
+
+COSTELLO "Look, all I wanna know is when you sign up the first baseman, how does he sign his name?"
+
+ABBOT "Who."
+
+COSTELLO "The guy."
+
+ABBOT "Who."
+
+COSTELLO "How does he sign..."
+
+ABBOT "That's how he signs it."
+
+COSTELLO "Who?"
+
+ABBOT "Yes."
+
+PAUSE
+
+COSTELLO "All I'm trying to find out is what's the guy's name on first base."
+
+ABBOT "No, what is on second base."
+
+COSTELLO "I'm not asking you who's on second."
+
+ABBOT "Who's on first."
+
+COSTELLO "One base at a time!"
+
+ABBOT "Well, don't change the players around."
+
+COSTELLO "I'm not changing nobody!"
+
+ABBOT "Take it easy, buddy."
+
+COSTELLO "I'm only asking you, who's the guy on first base?"
+
+ABBOT "That's right."
+
+COSTELLO "Ok."
+
+ABBOT "All right."
+
+PAUSE
+
+COSTELLO "What's the guy's name on first base?"
+
+ABBOT "No, what is on second."
+
+COSTELLO "I'm not asking you who's on second."
+
+ABBOT "Who's on first."
+
+COSTELLO "I don't know."
+
+ABBOT "He's on third, we're not talking about him."
+
+COSTELLO "Now how did I get on third base?"
+
+ABBOT "Why you mentioned his name."
+
+COSTELLO "If I mentioned the third baseman's name, who did I say is playing third?"
+
+ABBOT "No, who's playing first."
+
+COSTELLO "What's on first?"
+
+ABBOT "What's on second."
+
+COSTELLO "I don't know."
+
+ABBOT "He's on third."
+
+COSTELLO "There I go, back on third again!"
+
+PAUSE
+
+COSTELLO "Would you just stay on third base and don't go off it."
+
+ABBOT "All right, what do you want to know?"
+
+COSTELLO "Now who's playing third base?"
+
+ABBOT "Why do you insist on puting Who on third base?"
+
+COSTELLO "What am I puting on third."
+
+ABBOT "No, what is on second."
+
+COSTELLO "You don't want who on second?"
+
+ABBOT "Who is on first."
+
+COSTELLO "I don't know."
+
+ABBOT "Third base!"
+
+PAUSE
+
+COSTELLO "Look, you gotta outfield?"
+
+ABBOT "Sure."
+
+COSTELLO "The left fielder's name?"
+
+ABBOT "Why."
+
+COSTELLO "I just thought I'd ask you."
+
+ABBOT "Well, I just thought I'd tell you."
+
+COSTELLO "Then tell me who's playing left field."
+
+ABBOT "Who's playing first."
+
+COSTELLO "I'm not... stay out of the infield! I want to know what's the guy's name in left field?"
+
+ABBOT "No, What is on second."
+
+COSTELLO "I'm not asking you who's on second."
+
+ABBOT "Who's on first!"
+
+COSTELLO "I don't know."
+
+ABBOT "Third base!"
+
+PAUSE
+
+COSTELLO "The left fielder's name?"
+
+ABBOT "Why."
+
+COSTELLO "Because!"
+
+ABBOT "Oh, he's centerfield."
+
+PAUSE
+
+COSTELLO "Look, You gotta pitcher on this team?"
+
+ABBOT "Sure."
+
+COSTELLO "The pitcher's name?"
+
+ABBOT "Tomorrow."
+
+COSTELLO "You don't want to tell me today?"
+
+ABBOT "I'm telling you now."
+
+COSTELLO "Then go ahead."
+
+ABBOT "Tomorrow!"
+
+COSTELLO "What time?"
+
+ABBOT "What time what?"
+
+COSTELLO "What time tomorrow are you gonna tell me who's pitching?"
+
+ABBOT "Now listen. Who is not pitching."
+
+COSTELLO "I'll break your arm, you say who's on first! I want to know what's the pitcher's name?"
+
+ABBOT "What's on second."
+
+COSTELLO "I don't know."
+
+ABBOT "Third base!"
+
+PAUSE
+
+COSTELLO "Got a catcher?"
+
+ABBOT "Certainly."
+
+COSTELLO "The catcher's name?"
+
+ABBOT "Today."
+
+COSTELLO "Today, and tomorrow's pitching."
+
+ABBOT "Now you've got it."
+
+COSTELLO "All we got is a couple of days on the team."
+
+PAUSE
+
+COSTELLO "You know I'm a catcher too."
+
+ABBOT "So they tell me."
+
+COSTELLO "I get behind the plate to do some fancy catching, Tomorrow's pitching on my team and a heavy hitter gets up. Now the heavy hitter bunts the ball. When he bunts the ball, me, being a good catcher, I'm gonna throw the guy out at first base. So I pick up the ball and throw it to who?"
+
+ABBOT "Now that's the first thing you've said right."
+
+COSTELLO "I don't even know what I'm talking about!"
+
+PAUSE
+
+ABBOT "That's all you have to do."
+
+COSTELLO "Is to throw the ball to first base."
+
+ABBOT "Yes!"
+
+COSTELLO "Now who's got it?"
+
+ABBOT "Naturally."
+
+PAUSE
+
+COSTELLO "Look, if I throw the ball to first base, somebody's gotta get it. Now who has it?"
+
+ABBOT "Naturally."
+
+COSTELLO "Who?"
+
+ABBOT "Naturally."
+
+COSTELLO "Naturally?"
+
+ABBOT "Naturally."
+
+COSTELLO "So I pick up the ball and I throw it to Naturally."
+
+ABBOT "No you don't, you throw the ball to Who."
+
+COSTELLO "Naturally."
+
+ABBOT "That's different."
+
+COSTELLO "That's what I said."
+
+ABBOT "You're not saying it..."
+
+COSTELLO "I throw the ball to Naturally."
+
+ABBOT "You throw it to Who."
+
+COSTELLO "Naturally."
+
+ABBOT "That's it."
+
+COSTELLO "That's what I said!"
+
+ABBOT "You ask me."
+
+COSTELLO "I throw the ball to who?"
+
+ABBOT "Naturally."
+
+COSTELLO "Now you ask me."
+
+ABBOT "You throw the ball to Who?"
+
+COSTELLO "Naturally."
+
+ABBOT "That's it."
+
+COSTELLO "Same as you! Same as YOU! I throw the ball to who. Whoever it is drops the ball and the guy runs to second. Who picks up the ball and throws it to What. What throws it to I Don't Know. I Don't Know throws it back to Tomorrow, Triple play. Another guy gets up and hits a long fly ball to Because. Why? I don't know! He's on third and I don't give a darn!"
+
+ABBOT "What?"
+
+COSTELLO "I said I don't give a darn!"
+
+ABBOT "Oh, that's our shortstop."
